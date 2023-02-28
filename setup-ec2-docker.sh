74ccf396990bd19b37851eec7c61737dff89a5b3
yangyou-fcj7 <82619524+yangyou-fcj7@users.noreply.github.com>
2023-02-28 20:41:37 +0900

    Update setup-ec2-docker.sh

--- setup-ec2-docker.sh
+++ setup-ec2-docker.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+# install docker 
 sudo amazon-linux-extras install -y docker
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
+
+# install docker compose cli
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
